ad0709983008faeee5f52a2741acbc1a9ee6df7c
John Nowinski <54250100+jnowinski@users.noreply.github.com>
2025-03-19 14:25:36 -0400

    Update robot_handler.py
    added seperate topics for each robot mode. added threading lock for velocity variable

--- robot_handler.py
+++ robot_handler.py
@@ -1,5 +1,6 @@
 import rospy
 import json
+import threading
 from std_msgs.msg import String
 from kinova_msgs.msg import PoseVelocity
 from std_srvs.srv import Empty
@@ -22,8 +23,14 @@ class KinovaStateMachine:
         # Store velocity for continuous publishing
         self.velocity = None
 
-        # Subscribe to the command topic
-        self.command_sub = rospy.Subscriber("send_robot_command", String, self.command_callback)
+        # Lock for synchronizing access to velocity
+        self.velocity_lock = threading.Lock()
+
+        # Subscribe to the command topics
+        self.command_sub_home = rospy.Subscriber("send_home", String, self.home_callback)
+        self.command_sub_velocity = rospy.Subscriber("update_velocity", PoseVelocity, self.velocity_callback)
+        self.command_sub_geofence_start = rospy.Subscriber("start_geofence", String, self.geofence_start_callback)
+        self.command_sub_geofence_stop = rospy.Subscriber("stop_geofence", String, self.geofence_stop_callback)
 
         # Publisher for velocity commands
         self.velocity_pub = rospy.Publisher("/j2n6s300_driver/in/cartesian_velocity", PoseVelocity, queue_size=10)
@@ -36,62 +43,35 @@ class KinovaStateMachine:
         # Start the continuous velocity publishing loop
         self.publish_velocity()
 
-    def command_callback(self, msg):
-        """Handles incoming JSON commands and performs appropriate actions."""
-        try:
-            command_data = json.loads(msg.data)  # Parse JSON
-            cmd_type = command_data.get("cmd_type")
+    def home_callback(self, msg):
+        """Sets the robot mode to home and homes the robot."""
+        rospy.loginfo("Received home command")
+        self.current_state = RobotState.HOME
+        rospy.wait_for_service("/j2n6s300_driver/in/home_arm")
+        home_service = rospy.ServiceProxy("/j2n6s300_driver/in/home_arm", Empty)
+        home_service()
+        rospy.loginfo("Robot moved to home position")
+        self.current_state = RobotState.IDLE  # Move back to IDLE after homing
 
-            if cmd_type == "velocity":
-                self.current_state = RobotState.VELOCITY
-                self.velocity = command_data.get("velocity")  # Update velocity
-                rospy.loginfo("Received velocity command: %s", self.velocity)
+    def velocity_callback(self, msg):
+        """Sets the robot mode to velocity and updates the velocity variable."""
+        rospy.loginfo("Received velocity command: %s", msg)
+        with self.velocity_lock:
+            self.velocity = msg
+        self.current_state = RobotState.VELOCITY
 
-            elif cmd_type == "home":
-                self.current_state = RobotState.HOME
-                rospy.wait_for_service("/j2n6s300_driver/in/home_arm")
-                home_service = rospy.ServiceProxy("/j2n6s300_driver/in/home_arm", Empty)
-                home_service()
-                rospy.loginfo("Robot moved to home position")
-                self.current_state = RobotState.IDLE  # Move back to IDLE after homing
+    def geofence_start_callback(self, msg):
+        """Sets the robot mode to geofence."""
+        rospy.loginfo("Received geofence start command")
+        self.current_state = RobotState.GEOFENCE
+        rospy.wait_for_service("/j2n6s300_driver/in/start_force_control")
+        geofence_service = rospy.ServiceProxy("/j2n6s300_driver/in/start_force_control", Empty)
+        geofence_service()
+        rospy.loginfo("Geofence enabled")
 
-            elif cmd_type == "geofence_on":
-                self.current_state = RobotState.GEOFENCE
-                rospy.wait_for_service("/j2n6s300_driver/in/start_force_control")
-                geofence_service = rospy.ServiceProxy("/j2n6s300_driver/in/start_force_control", Empty)
-                geofence_service()
-                rospy.loginfo("Geofence enabled")
-
-            elif cmd_type == "geofence_off":
-                self.current_state = RobotState.IDLE
-                rospy.wait_for_service("/j2n6s300_driver/in/stop_force_control")
-                geofence_service = rospy.ServiceProxy("/j2n6s300_driver/in/stop_force_control", Empty)
-                geofence_service()
-                rospy.loginfo("Geofence disabled, returning to home mode")
-
-            else:
-                rospy.logwarn("Unknown command type received: %s", cmd_type)
-
-        except json.JSONDecodeError:
-            rospy.logwarn("Invalid JSON received: %s", msg.data)
-
-    def publish_velocity(self):
-        """Continuously publish velocity updates at 100 Hz if in VELOCITY state."""
-        while not rospy.is_shutdown():
-            if self.current_state == RobotState.VELOCITY and self.velocity:
-                vel_msg = PoseVelocity()
-                vel_msg.twist_linear_x = self.velocity.get("twist_linear_x", 0.0)
-                vel_msg.twist_linear_y = self.velocity.get("twist_linear_y", 0.0)
-                vel_msg.twist_linear_z = self.velocity.get("twist_linear_z", 0.0)
-                vel_msg.twist_angular_x = self.velocity.get("twist_angular_x", 0.0)
-                vel_msg.twist_angular_y = self.velocity.get("twist_angular_y", 0.0)
-                vel_msg.twist_angular_z = self.velocity.get("twist_angular_z", 0.0)
-
-                self.velocity_pub.publish(vel_msg)
-                rospy.loginfo("Published velocity: %s", vel_msg)
-
-            self.rate.sleep()  # Maintain 100 Hz publishing rate
-
-if __name__ == "__main__":
-    KinovaStateMachine()
-    rospy.spin()
+    def geofence_stop_callback(self, msg):
+        """Sets the robot mode to idle and stops geofence."""
+        rospy.loginfo("Received geofence stop command")
+        self.current_state = RobotState.IDLE
+        rospy.wait_for_service("/j2n6s300_driver/in/stop_force_control")
+        geofence_service = rospy.ServiceProxy("/j2n6s300_driver/in/stop_force_control", E
